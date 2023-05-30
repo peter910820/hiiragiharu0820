b2dc0b40cc09eba4a1fb4972d2371795fd35a0d1
peter910820 <peter910820@gmail.com>
2023-05-31 00:08:42 +0800

    test load sequence3.

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,1 +1,9 @@
+window.addEventListener("load", function() {
+  var loadingScreen = document.getElementById("loading-screen");
+  var content = document.getElementById("hidden");
+  
+  loadingScreen.style.display = "none";
+  content.classList.remove("hidden");
+});
+
 new WOW().init();
